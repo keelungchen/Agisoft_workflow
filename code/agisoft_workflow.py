4aa76948542d3757f9b050fea507f3e052349405
keelungchen <keelungchen999@gmail.com>
2024-12-03 01:46:10 -1000

    first commit

---
+++ code/agisoft_workflow.py
@@ -0,0 +1,7 @@
+import Metashape
+import os
+
+# === 參數設定 ===
+photo_folder = r"D:\3D_workshop\indoor_demo\tg_ortho_20\photos"  # 照片位置
+output_folder = r"D:\3D_workshop\indoor_demo\tg_ortho_20\agisoft"  # 儲存專案的位置
+project_name = "tg_ortho_20"  # 專案名稱
